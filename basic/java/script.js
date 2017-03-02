c0e16cbcdb67750bfe7b4f7b4eb356570fed7ec4
tdeboard <tdeboard@students.mchenry.edu>
2017-03-01 20:42:57 -0600

    Add files via upload

---
+++ basic/java/script.js
@@ -0,0 +1,23 @@
+function change01(){
+document.getElementById("large").src="https://aos.iacpublishinglabs.com/question/aq/1400px-788px/why-do-light-bulbs-explode_cdfed3fd-f374-4fd8-a2ee-8ce04274ec15.jpg?domain=cx.aos.ask.com";
+}
+
+function change02(){
+document.getElementById("large").src="https://s-media-cache-ak0.pinimg.com/originals/82/ff/b3/82ffb3682eb85f8459a73e05a46da398.jpg";
+}
+
+function change03(){
+document.getElementById("large").src="http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/lightbulbs-primary.png";
+}
+
+function change04(){
+document.getElementById("large").src="http://i.huffpost.com/gen/1776994/images/o-LIGHTBULBS-facebook.jpg";
+}
+
+function change05(){
+document.getElementById("large").src="http://www.anh-usa.org/wp-content/uploads/2014/12/light-bulbs2.jpg";
+}
+
+function change06(){
+document.getElementById("large").src="http://www.designboom.com/weblog/images/images_2/lara/818_images/bulbrite/bulbrite04.jpg";
+}
